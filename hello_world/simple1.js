1bfb34d54d47e0a2420f8f85f286726fc8f19767
Seungdols <seungdols0822@gmail.com>
2019-06-29 15:36:59 +0900

    Add count

--- hello_world/simple1.js
+++ hello_world/simple1.js
@@ -13,5 +13,24 @@ class LikeButton extends React.Component {
 
 }
 
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  render() {
+    return React.createElement(
+      'div', null, React.createElement(LikeButton), 
+      React.createElement('div', { style: { marginTop: 20}}, 
+      React.createElement('span', null, '현재 카운트: '), 
+      React.createElement('span', null, this.state.count),
+      React.createElement('button', { onClick: () => this.setState({ count: this.state.count + 1})}, '증가'),
+      React.createElement('button', { onClick: () => this.setState({ count: this.state.count - 1})}, '감소'),
+      )
+    );
+  }
+}
+
 const domContainer = document.querySelector('#react-root');
-ReactDOM.render(React.createElement(LikeButton), domContainer);
+ReactDOM.render(React.createElement(Container), domContainer);
