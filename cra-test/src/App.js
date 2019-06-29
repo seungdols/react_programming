7d6b0654cad1f56310d01eaa33ca02524a606b22
Seungdols <seungdols0822@gmail.com>
2019-06-29 16:26:57 +0900

    Use CRA and Dynamic import

---
+++ cra-test/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './TodoList'
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
