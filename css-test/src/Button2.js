b606eb16747682436d9e56866cc0e067d859e8c2
seungdols <seungdols0822@gmail.com>
2019-08-05 20:14:02 +0900

    Apply classnames

--- css-test/src/Button2.js
+++ css-test/src/Button2.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './Button2.module.css'
+import cn from 'classnames'
 
 function Button({size}) {
   if(size === 'big') {
-    return <button className={`${style.button} ${style.big}`}>Big button</button>;
+    return <button className={cn(style.button, style.big)}>Big button</button>;
   } else {
-    return <button className={`${style.button} ${style.small}}`}>small button</button>;
+    return <button className={cn(style.button, style.small)}>small button</button>;
   }
 }
 
