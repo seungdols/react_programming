ad26903bebdd934638c226adb94b1d38b80dc42f
seungdols <seungdols0822@gmail.com>
2019-08-07 19:50:31 +0900

    Modify simple SPA

--- router-test/src/App.js
+++ router-test/src/App.js
@@ -3,20 +3,54 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    pageName: '',
+  }
   componentDidMount() {
-    window.onpopstate = function(event) {
-      console.log(`location: ${document.location}, state: ${event.state}`)
+    window.onpopstate = event => {
+      this.onChangePage(event.state)
     }
   }
 
+  onChangePage = pageName => {
+    this.setState({ pageName })
+  }
+
+  onClick1 = () => {
+    const pageName = 'page1'
+    window.history.pushState(pageName, '', '/page1')
+    this.onChangePage(pageName)
+  }
+
+  onClick2 = () => {
+    const pageName = 'page2'
+    window.history.pushState(pageName, '', '/page2')
+    this.onChangePage(pageName)
+  }
   render() {
+    const { pageName } = this.state
     return (
       <div>
-        <button onClick={() => window.history.pushState('v1', '', '/page1')}>page1</button>
-        <button onClick={() => window.history.pushState('v2', '', '/page2')}>page2</button>
+        <button onClick={this.onClick1}>page1</button>
+        <button onClick={this.onClick2}>page2</button>
+        {!pageName && <Home />}
+        {pageName === 'page1' && <Page1 />}
+        {pageName === 'page2' && <Page2 />}
       </div>
     )
   }
+}
+
+function Home() {
+  return <h2>This is Homepage. Be page click</h2>
+}
+
+function Page1() {
+  return <h2>This is page1.</h2>
+}
+
+function Page2() {
+  return <h2>This is page2.</h2>
 }
 
 export default App;
